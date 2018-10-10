6298a92a9b9f97b6185593c31ea4ad56ba4dfe27
Will <jwilt9000@gmail.com>
2018-10-09 17:24:46 -0700

    Edited code to allow correct driving

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DemoOPModeAdvanced.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DemoOPModeAdvanced.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -11,7 +12,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 @TeleOp(name = "DemoTeleOpModeAdvanced", group = "Demo")
-@Disabled
 public class DemoOPModeAdvanced extends OpMode {
 
     //All variables are declared with the modifier "private"
@@ -27,16 +27,17 @@ public class DemoOPModeAdvanced extends OpMode {
     //Always separate each set of variable types... it just looks nicer
     private Servo DemoServo;
 
+    private ColorSensor DemoColorSensor;
+
     //Code that runs when the init button is pressed (Eg. Assign all variables)
     public void init(){
         telemetry.addData("Robot Status: ", "Initializing");
 
         //The following assigns the corresponding motor variable to the  variable name on the phone which corresponds to a motor connection # on the REV hub
         //!!!IMPORTANT!!! If the names are changed on the phones such as "MotorLF" is changed to "MotorLeftFront" the code following must be updated
-        MotorLeftFront = hardwareMap.get(DcMotor.class, "MotorLF");
-        MotorRightFront = hardwareMap.get(DcMotor.class, "MotorRF");
         MotorLeftBack = hardwareMap.get(DcMotor.class, "MotorLB");
         MotorRightBack = hardwareMap.get(DcMotor.class, "MotorRB");
+        DemoColorSensor = hardwareMap.get(ColorSensor.class, "ColorSensor");
 
         //Assigns each motor to a list to allow for the values to change easier using a for loop
         Motors.add(MotorLeftFront);
@@ -44,11 +45,8 @@ public class DemoOPModeAdvanced extends OpMode {
         Motors.add(MotorLeftBack);
         Motors.add(MotorRightBack);
 
-        DemoServo = hardwareMap.get(Servo.class, "DemoServo");
-
         //Needs to be reversed to move forward
-        MotorLeftBack.setDirection(DcMotorSimple.Direction.REVERSE);
-        MotorLeftFront.setDirection(DcMotorSimple.Direction.REVERSE);
+
 
         telemetry.addData("Robot Status: ", "Initialized");
     }
@@ -60,9 +58,9 @@ public class DemoOPModeAdvanced extends OpMode {
     public void loop(){
         //Should in theory work, however I haven't touched this code in a year so it might take some time to get this working again
         //Basically what this does is set the power of the motor to the y value on the stick corresponding to that side thus creating tank drive(In theory)
-        MotorRightFront.setPower(gamepad1.right_stick_y);
-        MotorRightBack.setPower(gamepad1.right_stick_y);
-        MotorLeftFront.setPower(gamepad1.left_stick_y);
+        //MotorRightFront.setPower(gamepad1.right_stick_y);
+        MotorRightBack.setPower(-gamepad1.right_stick_y);
+        //MotorLeftFront.setPower(gamepad1.left_stick_y);
         MotorLeftBack.setPower(gamepad1.left_stick_y);
 
 
@@ -70,9 +68,7 @@ public class DemoOPModeAdvanced extends OpMode {
         //A motors power can be set to anything between 0 and 1, because it accepts a value(variable) type called a double(floats work similar)
         if(gamepad1.dpad_up)
         {
-            MotorRightFront.setPower(1);
             MotorRightBack.setPower(1);
-            MotorLeftFront.setPower(1);
             MotorLeftBack.setPower(1);
         }
 
@@ -87,17 +83,17 @@ public class DemoOPModeAdvanced extends OpMode {
         //region Servo Controller
         //A servos position can range between 0 and 1 and works similar to the motors however it can be set to an exact spot as opposed to go, at this speed
         //The following code will increment the current servo position by 0.05 while it is pressed
-        if (gamepad1.right_bumper)
-        {
-            DemoServo.setPosition(DemoServo.getPosition() + 0.05);
-        }
-
-
-        //This will lower the servo when left bumper is pressed
-        if(gamepad1.left_bumper)
-        {
-            DemoServo.setPosition(DemoServo.getPosition() - 0.05);
-        }
+       // if (gamepad1.right_bumper)
+       // {
+       //     DemoServo.setPosition(DemoServo.getPosition() + 0.05);
+       // }
+//
+//
+       // //This will lower the servo when left bumper is pressed
+       // if(gamepad1.left_bumper)
+       // {
+       //     DemoServo.setPosition(DemoServo.getPosition() - 0.05);
+       // }
         //endregion
     }
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DemoOPModeBasic.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DemoOPModeBasic.java
@@ -11,6 +11,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -30,22 +31,25 @@ public class DemoOPModeBasic extends LinearOpMode {
     //Always separate each set of variable types... it just looks nicer
     private Servo DemoServo;
 
+    private ColorSensor DemoColorSensor;
+
     //Where the majority of your code will go some variables are defined above this outside the runOpMode method
     public void runOpMode()
     {
 
         //The following assigns the corresponding motor variable to the  variable name on the phone which corresponds to a motor connection # on the REV hub
         //!!!IMPORTANT!!! If the names are changed on the phones such as "MotorLF" is changed to "MotorLeftFront" the code following must be updated
-        MotorLeftFront = hardwareMap.get(DcMotor.class, "MotorLF");
-        MotorRightFront = hardwareMap.get(DcMotor.class, "MotorRF");
+        //MotorLeftFront = hardwareMap.get(DcMotor.class, "MotorLF");
+        //MotorRightFront = hardwareMap.get(DcMotor.class, "MotorRF");
         MotorLeftBack = hardwareMap.get(DcMotor.class, "MotorLB");
         MotorRightBack = hardwareMap.get(DcMotor.class, "MotorRB");
 
-        DemoServo = hardwareMap.get(Servo.class, "DemoServo");
+        //DemoServo = hardwareMap.get(Servo.class, "DemoServo");
 
+        DemoColorSensor = hardwareMap.get(ColorSensor.class, "ColorSensor");
         //Needs to be reversed to move forward
-        MotorLeftBack.setDirection(DcMotorSimple.Direction.REVERSE);
-        MotorLeftFront.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        //MotorLeftFront.setDirection(DcMotorSimple.Direction.REVERSE);
 
         //After initialization of variables has completed wait until the game starts(The play button is pressed)
         waitForStart();
@@ -56,21 +60,21 @@ public class DemoOPModeBasic extends LinearOpMode {
         {
             //Should in theory work, however I haven't touched this code in a year so it might take some time to get this working again
             //Basically what this does is set the power of the motor to the y value on the stick corresponding to that side thus creating tank drive(In theory)
-            MotorRightFront.setPower(gamepad1.right_stick_y);
-            MotorRightBack.setPower(gamepad1.right_stick_y);
-            MotorLeftFront.setPower(gamepad1.left_stick_y);
+            //MotorRightFront.setPower(gamepad1.right_stick_y);
+            MotorRightBack.setPower(-gamepad1.right_stick_y);
+            //MotorLeftFront.setPower(gamepad1.left_stick_y);
             MotorLeftBack.setPower(gamepad1.left_stick_y);
 
 
             //Alternatively you can set the motor power using a float as so...
             //A motors power can be set to anything between 0 and 1, because it accepts a value(variable) type called a double(floats work similar)
-            if(gamepad1.dpad_up)
-            {
-                MotorRightFront.setPower(1);
-                MotorRightBack.setPower(1);
-                MotorLeftFront.setPower(1);
-                MotorLeftBack.setPower(1);
-            }
+            //if(gamepad1.dpad_up)
+            //{
+            //    //MotorRightFront.setPower(1);
+            //    MotorRightBack.setPower(1);
+            //    //MotorLeftFront.setPower(1);
+            //    MotorLeftBack.setPower(1);
+            //}
 
 
             //Will be called if A on controller 1 is pressed
@@ -83,17 +87,17 @@ public class DemoOPModeBasic extends LinearOpMode {
             //region Servo Controller
             //A servos position can range between 0 and 1 and works similar to the motors however it can be set to an exact spot as opposed to go, at this speed
             //The following code will increment the current servo position by 0.05 while it is pressed
-            if (gamepad1.right_bumper)
-            {
-                DemoServo.setPosition(DemoServo.getPosition() + 0.05);
-            }
-
-
-            //This will lower the servo when left bumper is pressed
-            if(gamepad1.left_bumper)
-            {
-                DemoServo.setPosition(DemoServo.getPosition() - 0.05);
-            }
+            //if (gamepad1.right_bumper)
+            //{
+            //    DemoServo.setPosition(DemoServo.getPosition() + 0.05);
+            //}
+//
+//
+            ////This will lower the servo when left bumper is pressed
+            //if(gamepad1.left_bumper)
+            //{
+            //    DemoServo.setPosition(DemoServo.getPosition() - 0.05);
+            //}
             //endregion
 
         }
