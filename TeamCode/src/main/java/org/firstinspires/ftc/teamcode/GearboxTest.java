0b08947a4526032f604f5cc0364bbcaf9a485f86
lopet576 <lopet576@hsd.k12.or.us>
2018-12-13 15:48:33 -0800

    Added screw to gearbox test code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GearboxTest.java
@@ -0,0 +1,39 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+
+@TeleOp(name = "GearboxTest", group = "DeLorean")
+public class GearboxTest extends LinearOpMode {
+    private final int REV_TICK_COUNT = 560;
+
+    private DcMotor motorL;
+    private DcMotor motorR;
+    private DcMotor screw;
+
+    public void runOpMode() {
+        motorL = hardwareMap.get(DcMotor.class, "motorL");
+        motorR = hardwareMap.get(DcMotor.class, "motorR");
+        screw = hardwareMap.get(DcMotor.class, "screw");
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            motorR.setPower(gamepad1.left_stick_y);
+            motorL.setPower(gamepad1.left_stick_y);
+
+            if (gamepad1.a){
+                screw.setPower(1);
+            }
+            if (gamepad1.b){
+                screw.setPower(0);
+            }
+
+
+        }
+
+    }
+}
