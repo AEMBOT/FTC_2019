71c643d5d6c5f8a1be5810b7b6af0782dbc7af99
lopet576 <lopet576@hsd.k12.or.us>
2019-01-25 17:17:22 -0800

    Over-commented teleop for notebook.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOp.java
@@ -1,10 +1,10 @@
 /**
- * Program written by Troy Lopez for DeLorean 4.0 as one of two
- * variants of automode to be used at qualifiers
+ * Program written by Troy Lopez and Zane OG for DeLorean 4.0 for Teleop to be used at qualifiers
  * 8 January, 2019
  */
 package org.firstinspires.ftc.teamcode;
 
+//Imports FTC code frames, auto fill, etc.
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -12,27 +12,32 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "MainTeleOp", group = "DeLorean")
 public class MainTeleOp extends LinearOpMode {
+    //This makes the motor variables.
     private DcMotor dcBackLeft;
     private DcMotor dcBackRight;
     private DcMotor dcFrontLeft;
     private DcMotor dcFrontRight;
-   // private DcMotor dcTuckLeft;
+    // This Motor doesn't exist with the new chassis; it's here to show process
+    // private DcMotor dcTuckLeft;
     private DcMotor dcIntake;
     private DcMotor dcConveyor;
     private DcMotor dcLift;
 
-    //Declare any other motors (servos, etc.)
+    //Declares any other motors (servos, etc.)
     private Servo svClaim;
     private Servo svConveyor;
     private Servo svSweeper;
 
-    // Max turn speed variable
+    //Max turn speed variable
+    //This was added because our drivers were crashing and to fast :(
     private double speedLimit;
 
-    //Declare sensors
+    //Sensors are declared here
+    //We don't have sensors! Thanks Mechanical :)
 
+    //This is what happens during initialization, before start.
     public void runOpMode() {
-        //Motor
+        //Motors are assigned to variables here
         dcBackLeft = hardwareMap.get(DcMotor.class, "BackLeft");
         dcBackRight = hardwareMap.get(DcMotor.class, "BackRight");
         dcFrontLeft = hardwareMap.get(DcMotor.class, "FrontLeft");
@@ -41,22 +46,27 @@ public class MainTeleOp extends LinearOpMode {
         dcConveyor = hardwareMap.get(DcMotor.class, "Conveyor");
         dcLift = hardwareMap.get(DcMotor.class, "Lift");
 
-        //Servo
+        //Servos also are assigned here
         svClaim = hardwareMap.get(Servo.class, "svClaim");
         svConveyor = hardwareMap.get(Servo.class, "svConveyor");
         svSweeper = hardwareMap.get(Servo.class, "svSweeper");
 
-        //Reverse left motors so forward is the same for all motors
+        //Reverses left motors so that forward is the same for all motors
         dcBackLeft.setDirection(DcMotor.Direction.REVERSE);
         dcFrontLeft.setDirection(DcMotor.Direction.REVERSE);
 
         //TODO: Implement speed limit for drivers
+        // This "TO DO" was done in a newer version, but is here to show process
+
         // Initialize speed limit
         speedLimit = 0.75;
 
+        //Hopefully this next line is self explanatory. Everything is paused until start is pressed.
         waitForStart();
 
+        //This maps each function to a motor, so drivers can drive.
         while (opModeIsActive()) {
+            //dcMotors are the motors, gamepad.___ is the button.
             dcBackLeft.setPower(gamepad1.left_stick_y);
             dcBackRight.setPower(gamepad1.right_stick_y);
             dcFrontLeft.setPower(gamepad1.left_stick_y);
@@ -66,7 +76,7 @@ public class MainTeleOp extends LinearOpMode {
             dcConveyor.setPower(gamepad2.right_trigger);
             dcLift.setPower(gamepad2.left_stick_y);
 
-
+            //When a is pressed a servo runs.
             if(gamepad1.a) {
                 svSweeper.setPosition(1);
             }
@@ -74,6 +84,7 @@ public class MainTeleOp extends LinearOpMode {
                 svSweeper.setPosition(0.5);
             }
 
+            //When b is pressed a servo runs.
             if(gamepad1.b) {
                 svConveyor.setPosition(1);
             }
@@ -81,12 +92,14 @@ public class MainTeleOp extends LinearOpMode {
                 svConveyor.setPosition(0.5);
             }
 
+            //When x is pressed a servo runs.
             if(gamepad1.x){
                 svClaim.setPosition(1);
             }
             else {
                 svClaim.setPosition(0);
             }
+            //Useless strafe code.
             /*
             if (gamepad1.dpad_left){
                 dcFrontLeft.setPower(-1);
